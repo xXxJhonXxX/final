ac1c03e199b206585c5bf62df39a3fab32373a22
xXxJhonXxX <153418853+xXxJhonXxX@users.noreply.github.com>
2023-12-15 22:58:21 +0800

    Update server.py

--- server.py
+++ server.py
@@ -154,4 +154,4 @@ def search_data():
     return render_template('table.html', entries=filtered_data)
 
 if __name__ == '__main__':
-   app.run(debug=True, host="0.0.0.0" port=5000)
+   app.run(debug=True, host="0.0.0.0", port=5000)
